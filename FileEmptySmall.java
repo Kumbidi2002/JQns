d2f141578f55d668a7bb91c1a4ba560aa33fc777
Kumbidi2002 <86557415+Kumbidi2002@users.noreply.github.com>
2024-07-17 18:21:53 +0530

    Create FileEmptySmall.java

---
+++ FileEmptySmall.java
@@ -0,0 +1,30 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Cell {
+
+    public static void main(String[] args) {
+        String filename = "C:\\Users\\arya2\\Desktop\\RGNIYD\\hi.txt";
+
+        try {
+            File file = new File(filename);
+            Scanner scanner = new Scanner(file);
+
+            if (!scanner.hasNextLine()) {
+                throw new Exception("Empty file");
+            }
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                System.out.println("Not Empty:"+line);
+            }
+
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
